9e93b9046a6bd8d1674320454bf27d8e306436b3
Le Tat Dinh <dinhprococoa@gmail.com>
2023-10-12 23:05:41 +0700

    init project

---
+++ ListServlet/src/main/java/app/TodoServlet.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/todoServlet")
+public class TodoServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("list.jsp").forward(req, resp);
+	}
+}
